aeab957902c6ff5679a2266b2f8a35ad46d8c597
saikiran12040 <saikirangen@gmail.com>
2022-11-09 21:38:57 +0530

    added files of packages

---
+++ scripts/package.js
@@ -0,0 +1,18 @@
+
+  import {packages} from "../components/data.js"
+  console.log(packages);
+
+ for (let i=0;i<packages.length;i++){
+  let pack=packages[i]
+  let item=pack[i]
+  console.log(item)
+  renderDom(item)
+ }
+ let renderDom=(item)=>{
+  let cont=document.getElementById('packagecontainer');
+  data.forEach((el)=>{
+      let card=productCard(el);
+      cont.append(card);
+
+  });
+};
